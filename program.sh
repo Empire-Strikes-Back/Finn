1cdac3d6198899f0bfe4cfdcb7f535b18aed3672
rename-trash-icon-on-ubuntu-20-04 <rename-trash-icon-on-ubuntu-20-04@program>
2021-08-01 04:12:04 +0300

    in program.sh i remove no-history push

--- program.sh
+++ program.sh
@@ -12,15 +12,4 @@ main(){
   clojure -M:main
 }
 
-push(){
-  ORIGIN=$(git remote get-url origin)
-  rm -rf .git
-  git init -b main
-  git remote add origin $ORIGIN
-  git config --local include.path ../.gitconfig
-  git add .
-  git commit -m "i am rename-trash-icon-on-ubuntu-20-04 program"
-  git push -f -u origin main
-}
-
 "$@"
